a9be21296b1deed09b91d7b8df49abb207953333
KadenFranklin <43596236+KadenFranklin@users.noreply.github.com>
2021-03-29 16:55:03 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
 #![cfg_attr(not(test), no_std)]
 
-use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, is_drawable};
+use pluggable_interrupt_os::vga_buffer::{BUFFER_WIDTH, BUFFER_HEIGHT, plot, ColorCode, Color, is_drawable, plot_num};
 use pc_keyboard::{DecodedKey, KeyCode};
 
 const HEIGHT: usize = 25;
 const WIDTH: usize = 80;
-const GAME_HEIGHT: usize = 23;
-const HEADER_SPACE: usize = 2;
 
 pub struct SpaceInvadersGame {
-    cells: [[Cell; WIDTH]; HEIGHT],
     character: Character,
+    cells: [[Cell; WIDTH]; HEIGHT],
     lasers: [Laser; 5],
     laser_count: usize,
-    space_invaders: [Invaders; 119],
+    space_invaders: [Invaders; 70],
     invader_count: usize,
     status: Status,
     score: usize
@@ -72,59 +70,39 @@ impl Position {
     }
 }
 
-#[derive(Debug,Copy,Clone,Eq,PartialEq)]
-pub struct Invaders {
-    pos: Position,
-    dir: Dir,
+#[derive(Copy,Debug,Clone,Eq,PartialEq)]
+pub struct Character {
+    col: usize,
+    row: usize,
+    dx: usize,
     active: bool
+
 }
 
-impl Invaders {
-    fn new(pos: Position) -> Self {
-        Invaders{
-            pos,
-            dir: Dir::E,
+impl Character {
+    pub fn new() -> Self {
+        Character {
+            col: BUFFER_WIDTH / 2,
+            row: BUFFER_HEIGHT - 3,
+            dx: BUFFER_WIDTH / 2,
             active: true
         }
     }
-
-    fn update_invader(&mut self) {
-        self.clear_invader();
-        self.increment_invader();
-        self.draw_invader();
+    fn update_character(&mut self) {
+        self.clear_current();
+        self.update_location();
+        self.draw_current();
     }
 
-    fn clear_invader(&self) {
-        if self.active { plot(' ', self.pos.col as usize, self.pos.row as usize, ColorCode::new(Color::Black, Color::Black)); }
+    fn clear_current(&self) { plot(' ', self.col, self.row, ColorCode::new(Color::Black, Color::Black)); }
+
+    fn update_location(&mut self) {
+        if self.dx <= 2 { self.dx = 2 }
+        if self.dx >= 78 { self.dx = 78 }
+        else{ self.col = self.dx; }
     }
 
-    fn draw_invader(&self) {
-        if self.active { plot('M', self.pos.col as usize, self.pos.row as usize, ColorCode::new(Color::Green, Color::Black)); }
-        }
-
-    fn check_bounds(&mut self) -> usize {
-        if self.pos.col <= 2 {return self.pos.col as usize }
-        if self.pos.col >= 78 {return self.pos.col as usize }
-        else{return self.pos.col as usize }
-    }
-
-    fn increment_invader(&mut self) {
-        if self.check_bounds() <= 2 {
-            self.pos.row += 1;
-            self.dir = self.dir.reverse();
-        }
-        if self.check_bounds() >= 78 {
-            self.pos.row += 1;
-            self.dir = self.dir.reverse();
-        }
-        else {
-            match self.dir {
-                Dir::E => self.pos.col += 1,
-                Dir::W => self.pos.col -= 1,
-                _ => {}
-            }
-        }
-    }
+    fn draw_current(&self) { plot('A', self.col, self.row, ColorCode::new(Color::Cyan, Color::Black)); }
 }
 
 #[derive(Debug,Copy,Clone,Eq,PartialEq)]
@@ -163,75 +141,92 @@ impl Laser {
     }
 }
 
-
-#[derive(Copy,Debug,Clone,Eq,PartialEq)]
-pub struct Character {
-    col: usize,
-    row: usize,
-    dx: usize,
+#[derive(Debug,Copy,Clone,Eq,PartialEq)]
+pub struct Invaders {
+    pos: Position,
+    dir: Dir,
     active: bool
-
 }
 
-impl Character {
-    pub fn new() -> Self {
-        Character {
-            col: BUFFER_WIDTH / 2,
-            row: BUFFER_HEIGHT - 3,
-            dx: BUFFER_WIDTH / 2,
+impl Invaders {
+    fn new(pos: Position) -> Self {
+        Invaders{
+            pos,
+            dir: Dir::E,
             active: true
         }
     }
-    fn update_character(&mut self) {
-        self.clear_current();
-        self.update_location();
-        self.draw_current();
+
+    fn update_invader(&mut self) {
+        self.clear_invader();
+        self.increment_invader();
+        self.draw_invader();
     }
 
-    fn clear_current(&self) { plot(' ', self.col, self.row, ColorCode::new(Color::Black, Color::Black)); }
-
-    fn update_location(&mut self) {
-        if self.dx <= 2 { self.dx = 2 }
-        if self.dx >= 78 { self.dx = 78 }
-        else{ self.col = self.dx; }
+    fn clear_invader(&self) {
+        if self.active { plot(' ', self.pos.col as usize, self.pos.row as usize, ColorCode::new(Color::Black, Color::Black)); }
     }
 
-    fn draw_current(&self) { plot('A', self.col, self.row, ColorCode::new(Color::Cyan, Color::Black)); }
+    fn draw_invader(&self) {
+        if self.active { plot('M', self.pos.col as usize, self.pos.row as usize, ColorCode::new(Color::Green, Color::Black)); }
+        }
+
+    fn increment_invader(&mut self) {
+        if self.pos.col <= 2 {
+            self.pos.row += 1;
+            self.dir = self.dir.reverse();
+        }
+        if self.pos.col >= 78 {
+            self.pos.row += 1;
+            self.dir = self.dir.reverse();
+        }
+        else {
+            if self.pos.row.is_even(){
+                self.pos.col += 1;
+            }
+            if self.pos.row.is_odd() {
+                self.pos.col -= 1;
+            }
+        }
+    }
 }
 
+const HEADER: &'static str =
+    "##                                 SpaceInvaders                               #
+#Score: 0                                                                      #";
 const START: &'static str =
-    "################################################################################\n
-    #                                                                              #\n
-    #     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #\n
-    #                                                                              #\n
-    #     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #\n
-    #                                                                              #\n
-    #     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #\n
-    #                                                                              #\n
-    #     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #\n
-    #                                                                              #\n
-    #                                                                              #\n
-    #                                                                              #\n
-    #                                                                              #\n
-    #                                                                              #\n
-    #                                                                              #\n
-    #      ******              ******              ******              ******      #\n
-    #    **********          **********          **********          **********    #\n
-    #    **********          **********          **********          **********    #\n
-    #    **********          **********          **********          **********    #\n
-    #                                                                              #\n
-    #                                      A                                       #\n
-    #                                                                              #\n
-    ################################################################################";
+    "################################################################################
+#                                                                              #
+#     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #
+#                                                                              #
+#     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #
+#                                                                              #
+#     M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M  M      #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+#       ****                ****                ****                ****       #
+#     ********            ********            ********            ********     #
+#    **********          **********          **********          **********    #
+#    **********          **********          **********          **********    #
+#                                                                              #
+#                                                                              #
+#                                                                              #
+################################################################################";
 
 impl SpaceInvadersGame {
     pub fn new() -> Self {
-        let mut game =SpaceInvadersGame {
-            cells: [[Cell::Empty; WIDTH]; HEIGHT],
+        let mut game = SpaceInvadersGame {
             character: Character::new(),
+            cells: [[Cell::Empty; WIDTH]; HEIGHT],
             lasers: [Laser::new(Position{col: 0, row: 0}); 5],
             laser_count: 0,
-            space_invaders: [Invaders::new(Position{col: 0, row: 0}); 119],
+            space_invaders: [Invaders::new(Position{col: 0, row: 0}); 70],
             invader_count: 0,
             status: Status::Running,
             score: 0
@@ -241,12 +236,13 @@ impl SpaceInvadersGame {
     }
 
     fn reset(&mut self) {
+        self.put_header();
         for (row, row_chars) in START.split('\n').enumerate() {
             for (col, icon) in row_chars.chars().enumerate() {
-                if icon == ' ' { plot(icon, col, row, ColorCode::new(Color::Black, Color::Black)) }
+                if icon == ' ' { plot(icon, col, row + 2, ColorCode::new(Color::Black, Color::Black)) }
                 else {
                     self.translate_icon(row, col, icon);
-                    plot(icon, col, row, ColorCode::new(Color::Blue, Color::Black))
+                    plot(icon, col, row + 2, ColorCode::new(Color::Blue, Color::Black));
                 }
             }
         }
@@ -256,28 +252,40 @@ impl SpaceInvadersGame {
         self.score = 0;
     }
 
-    fn translate_icon(&mut self, rowe: usize, column: usize, icon: char) {
+    fn translate_icon(&mut self, row: usize, col: usize, icon: char) {
         match icon {
-            '#' => self.cells[rowe][column] = Cell::Wall,
-            '*' => self.cells[rowe][column] = Cell::Barricade,
-            ' ' => self.cells[rowe][column] = Cell::Empty,
+            '#' => self.cells[row][col] = Cell::Wall,
+            '*' => self.cells[row][col] = Cell::Barricade,
+            ' ' => self.cells[row][col] = Cell::Empty,
             'A' => self.character = Character::new(),
             'M' => {
-                if self.invader_count < 93 {
                     self.invader_count += 1;
-                    self.space_invaders[self.invader_count] = Invaders::new( Position{col: column as i16 ,row: rowe as i16 } );
-                }
+                    self.space_invaders[self.invader_count] = Invaders::new( Position{col: col as i16 ,row: row as i16 } );
             }
             _ =>  panic!("Unrecognized character: '{}'", icon)
         }
     }
 
+    fn put_header(&mut self) {
+        for (row, row_chars) in HEADER.split('\n').enumerate() {
+            for (col, icon) in row_chars.chars().enumerate() {
+                plot(icon, col, row, ColorCode::new(Color::White, Color::Black));
+            }
+        }
+    }
+
+    fn update_score(&mut self) {
+        self.score += 10;
+        plot_num(self.score as isize, 8, 1, ColorCode::new(Color::White, Color::Black));
+    }
+
     pub fn tick(&mut self) {
         self.character.update_character();
         self.increment_laser();
-        self.increment_invaders();
+        //rn doesnt work with increment_invaders()
+        //self.increment_invaders();
         self.check_collision();
-        //also need to update the score and header
+        self.update_score();
     }
 
     pub fn key(&mut self, key: DecodedKey) {
@@ -301,7 +309,7 @@ impl SpaceInvadersGame {
 
     fn handle_unicode(&mut self, key: char) {
         match self.status {
-            Status::Over => { if key == ' ' { self.reset() } },
+            Status::Over => { if key == ' ' { self.reset()} },
             _ => { if key == ' ' { self.shoot() } }
         }
     }
@@ -330,10 +338,14 @@ impl SpaceInvadersGame {
 
     fn check_collision(&mut self) {
         for laser in self.lasers.iter_mut() {
+            //check for collision with barricades or walls
+
             for invader in self.space_invaders.iter_mut(){
                 if (laser.pos.row == invader.pos.row && laser.pos.col == invader.pos.col) && (laser.active && invader.active) {
+                    plot(' ', laser.pos.col as usize, laser.pos.row as usize, ColorCode::new(Color::Black, Color::Black));
                     laser.active = false;
                     invader.active = false;
+                    self.score += 100;
                 }
             }
         }
